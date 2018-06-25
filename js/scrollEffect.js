1ce7cc569313425efd8a3617b3a0b99593046157
Norayr <norayr.ghukasian@gmail.com>
2018-06-26 01:12:53 +0400

    project education

---
+++ js/scrollEffect.js
@@ -0,0 +1,17 @@
+function getCoords() {
+	let navigation = document.querySelector('.header__navigation');
+	let heading = document.querySelector('h1');
+	let headingCoords = heading.getBoundingClientRect();
+	
+	if ( headingCoords.top < 150) {
+		navigation.classList.add('header__navigation_scrolled');
+	} else {
+		navigation.classList.remove('header__navigation_scrolled');
+	}
+}
+
+
+
+document.onscroll = function () {
+	getCoords();
+};
